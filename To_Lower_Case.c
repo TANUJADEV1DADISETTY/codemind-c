e078fd1c8875827497c898bdf94c65a28158c3da
TANUJADEV1DADISETTY <147792227+TANUJADEV1DADISETTY@users.noreply.github.com>
2023-12-23 10:20:01 +0530

    #include<stdio.h> int main() {     char str[20];     scanf("%[^ ]s",str);     int i;     for(i=0;str[i]!=NULL;i++)     {         if(str[i]>='A' && str[i]<='Z')         {             str[i]=str[i]+32;         }     }     printf("%s",str); }

--- To_Lower_Case.c
+++ To_Lower_Case.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
-//#include<string.h>
 int main()
 {
-    char str[101];
+    char str[20];
     scanf("%[^
 ]s",str);
     int i;
-    for(i=0;i<str[i]!=NULL;i++)
+    for(i=0;str[i]!=NULL;i++)
     {
-        if(str[i]>='A'&& str[i]<='Z')      
+        if(str[i]>='A' && str[i]<='Z')
         {
             str[i]=str[i]+32;
         }
